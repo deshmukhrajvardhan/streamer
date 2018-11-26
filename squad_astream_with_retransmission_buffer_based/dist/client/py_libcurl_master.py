2eabb9c254dce727b72b38c8020a2d483e460bc8
RAJVARDHAN SOMRAJ DESHMUKH <17696886+deshmukhrajvardhan@users.noreply.github.com>
2018-11-26 15:23:25 -0500

    Update py_libcurl_master.py

--- squad_astream_with_retransmission_buffer_based/dist/client/py_libcurl_master.py
+++ squad_astream_with_retransmission_buffer_based/dist/client/py_libcurl_master.py
@@ -1,110 +1,231 @@
 import sysv_ipc
+import time
 import threading
+from multiprocessing import Process, Queue
 import sys
 import errno
 import timeit
 from string import ascii_letters, digits
-import read_mpd
 import urllib
 import random
 import os
 
-def write_msg(list_cmd,mq):
-        for i in list_cmd:
-                print (i)
-                mq.send(i, True)
-        return
+
+def write_msg(list_cmd, mq):
+    for i in list_cmd:
+        print(i)
+        mq.send(i, True)
+    return
+
+def read_ipc(segment_url):
+
+    key_c_orig_w = 262145
+
+    try:
+        mq_orig_r = sysv_ipc.MessageQueue(key_c_orig_w, sysv_ipc.IPC_CREAT, max_message_size=15000)
+    except:
+        print("ERROR: mq_orig_r Queue not created")
+
+    chunk_number = 0
+    total_data_dl_time = 0
+    segment_chunk_rates=[]
+    segment_size=0
+    chunk_start_time = timeit.default_timer()
 
 
-def get_mpd(url):
+    while True:
+        (message, prior) = mq_orig_r.receive()
+        m = message.split(b':')
+        # print("Reply:{}, content-length:{}\n".format(message,m[1]))
+        # chunk_sizes.append(float(m[1]))
+        if m[0] != b'end':
+            segment_size += float(m[1])
+            timenow = timeit.default_timer()
+            chunk_dl_time = timenow - chunk_start_time
+            chunk_start_time = timenow
+            chunk_number += 1
+            total_data_dl_time += chunk_dl_time
+            current_chunk_dl_rate = segment_size * 8 / total_data_dl_time
+            segment_chunk_rates.append(current_chunk_dl_rate)
+        else:
+            break
+
+    # content_length = chunk_sizes.pop()
+    # DONE part
+    with open('/dev/SQUAD/3_segs_chunk_rate_squad_libcurl_HTTP2.txt', 'a') as chk:
+        chk.write("{}".format(segment_url))
+        for item in segment_chunk_rates:
+            chk.write(",{}".format(item))
+        chk.write("\n")
+    # print("{} sum:{}, content_length:{}".format(p_no,sum(chunk_sizes), content_length))
+
+def write_ipc(segment_url):
+    key_c_orig_r = 262144
+    cmd1 = [segment_url]
+
+    try:
+        mq_orig_w = sysv_ipc.MessageQueue(key_c_orig_r, sysv_ipc.IPC_CREAT, max_message_size=15000)
+    except:
+        print("ERROR: Queue not created")
+    
+    process3 = Process(target=write_msg, args=(cmd1, mq_orig_w))
+    process3.start()
+    process3.join()
+    time.sleep(0.25)
+
+
+
+def get_segment(segment_url):
     """ Module to download the MPD from the URL and save it to file"""
     key_c_orig_r = 262144
     key_c_orig_w = 262145
-    key_c_retx_r=362146
-    key_c_retx_w=462146
-    keyr1=562146
-    keyr2=662146
+#    key_c_retx_r = 362146
+#    key_c_retx_w = 462146
+    #print(segment_url)
+    cmd1 = [segment_url]
 
-############################################## send url ######################################
-
-
-    mpd_file = url.split('/')[-1]
-    mpd_file=os.path.abspath(mpd_file)
     try:
-        mq_orig_w=sysv_ipc.MessageQueue(key_c_orig_r, sysv_ipc.IPC_CREAT, max_message_size = 15000)
+        mq_orig_w = sysv_ipc.MessageQueue(key_c_orig_r, sysv_ipc.IPC_CREAT, max_message_size=15000)
     except:
-        print ("ERROR: Queue not created")
+        print("ERROR: Queue not created")
     try:
-        mq_retx_w=sysv_ipc.MessageQueue(key_c_retx_r, sysv_ipc.IPC_CREAT, max_message_size = 15000)
+        mq_orig_r = sysv_ipc.MessageQueue(key_c_orig_w, sysv_ipc.IPC_CREAT, max_message_size=15000)
     except:
-        print ("ERROR: Queue not created")
-    
-    #parse_url = urllib.parse.urlparse(url)
-    #cmd1=["CREATE_STREAM",parse_url.path,mpd_file]
-    cmd1=["CREATE_STREAM",url]
-    print (url)
-    #print (mpd_file)
-    #if "CONN_CREATED" in (str(message[0],'utf-8', errors='ignore').split('\x00')[0]):
-    if True:
-        print ("CONNECTION ESTABLISHED")
-        thread3=threading.Thread(target=write_msg,args=(cmd1,mq_orig_w))
-        thread3.start()
-    #    try:
-#              mqr1.remove()
- #             mqw1.remove()
-  #      except sysv_ipc.ExistentialError:
-   #           print ("Existen.. error")
-        #process3.join()
-        thread3.join()
-#        return mpd_file
+        print("ERROR: mq_orig_r Queue not created")
+
+    if segment_url =="-2":
+        process3 = Process(target=write_msg, args=(cmd1, mq_orig_w))
+        process3.start()
+        process3.join()
+        exit()
+
+    chunk_number = 0
+    total_data_dl_time = 0
+    segment_chunk_rates=[]
+    segment_size=0
+    process3 = Process(target=write_msg, args=(cmd1, mq_orig_w))
+    # thread3=threading.Thread(target=write_msg,args=(cmd1,mqw1))
+    # thread3.start()
+    process3.start()
+    chunk_start_time = timeit.default_timer()
+    # ipc read
+
+    # print("Py master reading chunks")
+    while True:
+        (message, prior) = mq_orig_r.receive()
+        m = message.split(b':')
+        # print("Reply:{}, content-length:{}\n".format(message,m[1]))
+        # chunk_sizes.append(float(m[1]))
+        if m[0] != b'end':
+            segment_size += float(m[1])
+            timenow = timeit.default_timer()
+            chunk_dl_time = timenow - chunk_start_time
+            chunk_start_time = timenow
+            chunk_number += 1
+            total_data_dl_time += chunk_dl_time
+            current_chunk_dl_rate = segment_size * 8 / total_data_dl_time
+            segment_chunk_rates.append(current_chunk_dl_rate)
+        else:
+            break
+
+    # content_length = chunk_sizes.pop()
+    # DONE part
+    with open('/dev/SQUAD/6_segs_chunk_rate_squad_libcurl_full_HTTP2.txt', 'a') as chk:
+        chk.write("{}".format(segment_url))
+        for item in segment_chunk_rates:
+            chk.write(",{}".format(item))
+        chk.write("\n")
+    # print("{} sum:{}, content_length:{}".format(p_no,sum(chunk_sizes), content_length))
+
+    process3.join()
+
+
+def get_retx_segment(segment_url):
+    """ Module to download the MPD from the URL and save it to file"""
+#    key_c_orig_r = 262144
+#    key_c_orig_w = 262145
+    key_c_retx_r = 362146
+    key_c_retx_w = 462146
+    #print(segment_url)
+    cmd1 = [segment_url]
+
+    try:
+        mq_retx_w = sysv_ipc.MessageQueue(key_c_retx_r, sysv_ipc.IPC_CREAT, max_message_size=15000)
+    except:
+        print("ERROR: Queue not created")
+    try:
+        mq_retx_r = sysv_ipc.MessageQueue(key_c_retx_w, sysv_ipc.IPC_CREAT, max_message_size=15000)
+    except:
+        print("ERROR: mq_orig_r Queue not created")
+
+    if segment_url =="-2":
+        process3 = Process(target=write_msg, args=(cmd1, mq_retx_w))
+        process3.start()
+        process3.join()
+        exit()
+
+    chunk_number = 0
+    total_data_dl_time = 0
+    segment_chunk_rates=[]
+    segment_size=0
+    process3 = Process(target=write_msg, args=(cmd1, mq_retx_w))
+    # thread3=threading.Thread(target=write_msg,args=(cmd1,mqw1))
+    # thread3.start()
+    process3.start()
+    chunk_start_time = timeit.default_timer()
+    # ipc read
+
+    # print("Py master reading chunks")
+    while True:
+        (message, prior) = mq_retx_r.receive()
+        m = message.split(b':')
+        # print("Reply:{}, content-length:{}\n".format(message,m[1]))
+        # chunk_sizes.append(float(m[1]))
+        if m[0] != b'end':
+            segment_size += float(m[1])
+            timenow = timeit.default_timer()
+            chunk_dl_time = timenow - chunk_start_time
+            chunk_start_time = timenow
+            chunk_number += 1
+            total_data_dl_time += chunk_dl_time
+            current_chunk_dl_rate = segment_size * 8 / total_data_dl_time
+            segment_chunk_rates.append(current_chunk_dl_rate)
+        else:
+            break
+
+    # content_length = chunk_sizes.pop()
+    # DONE part
+    with open('/dev/SQUAD/6_retx_segs_chunk_rate_squad_libcurl_full_HTTP2.txt', 'a') as chk:
+        chk.write("{}".format(segment_url))
+        for item in segment_chunk_rates:
+            chk.write(",{}".format(item))
+        chk.write("\n")
+    # print("{} sum:{}, content_length:{}".format(p_no,sum(chunk_sizes), content_length))
+
+    process3.join()
+
+def main(argv):
+    urls = ["https://10.10.3.2/www-itec.uni-klu.ac.at/ftp/datasets/DASHDataset2014/BigBuckBunny/2sec/bunny_4219897bps/BigBuckBunny_2s13.m4s",
+            "https://10.10.3.2/www-itec.uni-klu.ac.at/ftp/datasets/DASHDataset2014/BigBuckBunny/2sec/bunny_4219897bps/BigBuckBunny_2s14.m4s",
+            "https://10.10.3.2/www-itec.uni-klu.ac.at/ftp/datasets/DASHDataset2014/BigBuckBunny/2sec/bunny_4219897bps/BigBuckBunny_2s15.m4s",
+            "https://10.10.3.2/www-itec.uni-klu.ac.at/ftp/datasets/DASHDataset2014/BigBuckBunny/2sec/bunny_4219897bps/BigBuckBunny_2s16.m4s",
+            "https://10.10.3.2/www-itec.uni-klu.ac.at/ftp/datasets/DASHDataset2014/BigBuckBunny/2sec/bunny_4219897bps/BigBuckBunny_2s17.m4s",
+            "https://10.10.3.2/www-itec.uni-klu.ac.at/ftp/datasets/DASHDataset2014/BigBuckBunny/2sec/bunny_4219897bps/BigBuckBunny_2s18.m4s"]
+            
+    if argv[1] == 'r':
+        for i in urls:
+            read_ipc(i)
+    elif argv[1] == 'w':
+        for i in urls:
+            write_ipc(i)
+    elif argv[1] == 'retx':
+        for i in urls:
+            get_segment(i)
+            get_retx_segment(i)
     else:
-        return ("MPD error")
+        for i in urls:
+            get_segment(i)        
 
-
-############################################## read chunks ###################################
-    try:
-        mq_orig_r=sysv_ipc.MessageQueue(key_c_orig_w, sysv_ipc.IPC_CREAT, max_message_size = 15000)
-    except:
-        print ("ERROR: mq_orig_r Queue not created")
-
-    try:
-        mq_retx_r=sysv_ipc.MessageQueue(key_c_retx_w, sysv_ipc.IPC_CREAT, max_message_size = 15000)
-    except:
-        print ("ERROR: mq_orig_r Queue not created")
-
-    message='start'
-    m=['start']
-    total_chunk_sizes=[]
-    chunk_sizes=[]
-    while m[0] != b'end':
-        (message,prior)=mq_orig_r.receive()
-        m=message.split(b':')
-#        print("Reply:{}, content-length:{}\n".format(message,m[1]))
-        chunk_sizes.append(float(m[1]))
-
-    content_length = chunk_sizes.pop()
-    print("P1 sum:{}, content_length:{}".format(sum(chunk_sizes),content_length))
-  #  if sum(chunk_sizes) == content_length:
-  #      total_chunk_sizes.append(chunk_sizes)
-  #  else:
-  #      print("Not all chunks in pipe 1")
-
-    chunk_sizes=[]
-    message='start'
-    m=['start']
-    while m[0] != b'end':
-        (message,prior)=mq_retx_r.receive()
-        m=message.split(b':')
- #       print("retx-Reply:{}, content-length:{}\n".format(message,m[1]))
-        chunk_sizes.append(float(m[1]))
-
-    content_length = chunk_sizes.pop()
-    print("P2 sum:{}, content_length:{}".format(sum(chunk_sizes),content_length))
-
-#    if sum(chunk_sizes) == content_length:
-#        total_chunk_sizes.append(chunk_sizes)
-#    else:
-#        print("Not all chunks in pipe 2")
-        
-get_mpd(" http://10.10.3.2/www-itec.uni-klu.ac.at/ftp/datasets/DASHDataset2014/BigBuckBunny/2sec/BigBuckBunny_2s.mpd")
-
+if __name__ == '__main__':
+#get_mpd("https:
+    main(sys.argv)
