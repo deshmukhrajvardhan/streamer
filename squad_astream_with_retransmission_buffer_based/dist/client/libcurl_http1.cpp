7bf97b39c3adaeb500523fd6bbc1c0fe3df66906
RAJVARDHAN SOMRAJ DESHMUKH <17696886+deshmukhrajvardhan@users.noreply.github.com>
2019-01-20 13:03:20 -0500

    able to use same certificate on multiple servers

--- squad_astream_with_retransmission_buffer_based/dist/client/libcurl_http1.cpp
+++ squad_astream_with_retransmission_buffer_based/dist/client/libcurl_http1.cpp
@@ -267,6 +267,7 @@ int main(){
                 curl_easy_setopt(easy[ORIG_EASY], CURLOPT_CAINFO, "cert.pem");
                 curl_easy_setopt(easy[ORIG_EASY], CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
                 curl_easy_setopt(easy[ORIG_EASY], CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+		curl_easy_setopt(easy[ORIG_EASY], CURLOPT_SSL_VERIFYHOST, 0);
                 curl_easy_setopt(easy[ORIG_EASY], CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
                 curl_easy_setopt(easy[ORIG_EASY], CURLOPT_WRITEDATA, (void *)&chunk);
                 curl_multi_add_handle(multi_handle, easy[ORIG_EASY]);
